91ff8b24186f172ef9765e6bd693fddccf0056f4
Martín Stancanelli <mastanca14@gmail.com>
2019-08-03 15:54:09 -0300

    Create not equals class

---
+++ ARPB2Engine/Model/Operators/NotEquals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ARPB2Engine.Model.Operators
+{
+    public class NotEquals<T> : Operator
+    {
+        public ILiteral<T> LeftHand { get; set; }
+        public ILiteral<T> RightHand { get; set; }
+
+        public NotEquals()
+        {
+        }
+
+        public override bool Execute()
+        {
+            return !LeftHand.GetValue().Equals(RightHand.GetValue());
+        }
+    }
+}
