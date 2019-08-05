7dceb75dabc3869455baabd0d4df00b88d7dcd4c
Martín Stancanelli <mastanca14@gmail.com>
2019-08-04 22:53:43 -0300

    Create gte and lte classes

---
+++ ARPB2Engine/Model/Operators/LessThanEquals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ARPB2Engine.Model.Operators
+{
+    public class LessThanEquals<T> : Operator
+    {
+        public ILiteral<T> LeftHand { get; set; }
+        public ILiteral<T> RightHand { get; set; }
+
+        public LessThanEquals()
+        {
+        }
+
+        public override bool Execute()
+        {
+            if (LeftHand.Equals(typeof(Number)) || RightHand.Equals(typeof(Number)))
+            {
+                return (int)LeftHand.GetValue() <= (int)RightHand.GetValue();
+            }
+            else
+            {
+                // TODO: Return exception
+                return false;
+            }
+        }
+    }
+}
