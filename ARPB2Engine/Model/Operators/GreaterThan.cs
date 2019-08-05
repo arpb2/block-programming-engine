7e77c649a0b8a283140a027a7b92cabe0dfee22d
Martín Stancanelli <mastanca14@gmail.com>
2019-08-04 22:18:38 -0300

    Create gt and lt classes

---
+++ ARPB2Engine/Model/Operators/GreaterThan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ARPB2Engine.Model.Operators
+{
+    public class GreaterThan<T> : Operator
+    {
+        public ILiteral<T> LeftHand { get; set; }
+        public ILiteral<T> RightHand { get; set; }
+
+        public GreaterThan()
+        {
+        }
+
+        public override bool Execute()
+        {
+            if (LeftHand.Equals(typeof(Number)) || RightHand.Equals(typeof(Number)))
+            {
+                return (int)LeftHand.GetValue() > (int)RightHand.GetValue();
+            }
+            else
+            {
+                // TODO: Return exception
+                return false;
+            }
+        }
+    }
+}
