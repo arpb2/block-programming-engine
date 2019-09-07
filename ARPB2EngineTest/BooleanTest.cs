46698ccb670da60ea02a923cce6e010d41921519
Martín Stancanelli <mastanca14@gmail.com>
2019-09-06 23:48:10 -0300

    Add boolean test

---
+++ ARPB2EngineTest/BooleanTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ARPB2Engine.Model;
+using Xunit;
+
+namespace ARPB2EngineTest
+{
+    public class BooleanTest
+    {
+        [Fact]
+        public void TestBoolean()
+        {
+            ILiteral<ARPB2Engine.Model.Boolean> boolean = new ARPB2Engine.Model.Boolean(true);
+            Assert.True((bool)boolean.GetValue());
+        }
+    }
+}
