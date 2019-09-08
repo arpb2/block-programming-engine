d093d6e387a373d4c4f8dc0b4a9e04e492c45bee
Martín Stancanelli <mastanca14@gmail.com>
2019-09-08 15:39:33 -0300

    Add test for lt

---
+++ ARPB2EngineTest/LessThanTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ARPB2Engine.Model;
+using ARPB2Engine.Model.Operators;
+using Xunit;
+
+namespace ARPB2EngineTest
+{
+    public class LessThanTest
+    {
+        [Theory]
+        [InlineData(1, 2, true)]
+        [InlineData(100, 500, true)]
+        [InlineData(25, 14, false)]
+        public void TestGreaterThanEquals(int number1, int number2, bool expectedResult)
+        {
+            LessThan<Number> gt = new LessThan<Number>
+            {
+                LeftHand = new Number(number1),
+                RightHand = new Number(number2)
+            };
+            Assert.Equal(expectedResult, gt.Execute());
+        }
+    }
+
+}
