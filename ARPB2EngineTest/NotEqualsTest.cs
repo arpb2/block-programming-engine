ff040c748717447b406d1dfc266d57637be9dce7
Martín Stancanelli <mastanca14@gmail.com>
2019-09-08 16:01:22 -0300

    Add test for ne

---
+++ ARPB2EngineTest/NotEqualsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ARPB2Engine.Model.Operators;
+using Xunit;
+
+namespace ARPB2EngineTest
+{
+    public class NotEqualsTest
+    {
+        [Theory]
+        [InlineData("foo", "bar", true)]
+        [InlineData("asd", "wed", true)]
+        [InlineData("blue", "blue", false)]
+        public void TestNotEquals(String string1, String string2, bool expectedResult)
+        {
+            NotEquals<ARPB2Engine.Model.String> gt = new NotEquals<ARPB2Engine.Model.String>
+            {
+
+                LeftHand = new ARPB2Engine.Model.String(string1),
+                RightHand = new ARPB2Engine.Model.String(string2)
+            };
+
+
+            Assert.Equal(expectedResult, gt.Execute());
+        }
+    }
+}
