8b6e422cc22309e1af6f16af7856d65174a5a5aa
Martín Stancanelli <mastanca14@gmail.com>
2019-09-15 21:41:17 -0300

    For loop

---
+++ ARPB2Engine/Model/For.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ARPB2Engine.Model
+{
+    public class For
+    {
+        int counter;
+        int repetitions;
+
+        public delegate void DoStatement();
+
+        public For(int counter, int repetitions)
+        {
+            this.counter = counter;
+            this.repetitions = repetitions;
+        }
+
+        public void ExecuteHandler(DoStatement handler)
+        {
+            for (; this.counter < repetitions; this.counter++)
+            {
+                handler();
+            }
+        }
+    }
+}
---
+++ ARPB2EngineTest/ForTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ARPB2Engine.Model;
+using Xunit;
+
+namespace ARPB2EngineTest
+{
+    public class ForTest
+    {
+        [Fact]
+        public void TestFor()
+        {
+            int counter = 0;
+            int repetitions = 5;
+            For aLoop = new For(counter, repetitions);
+
+            int globalCheck = 0;
+
+            void CallMe5Times()
+            {
+                globalCheck++;
+            }
+
+            aLoop.ExecuteHandler(CallMe5Times);
+            Assert.Equal(globalCheck, 5);
+        }
+    }
+}
